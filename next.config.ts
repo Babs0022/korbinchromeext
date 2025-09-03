e30a0f113acc51b6e880ab0e2a13e6259d62e353
Babatunde Elijah <babseli933@gmail.com>
2025-09-03 07:51:48 +0000

    how do we get thing into chrome extension\

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,15 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'out',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  images: {},
 };
 
 export default nextConfig;
